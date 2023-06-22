a639ec2cc1a2b58183b51d9807db3261db71451a
Lukasz Momot <lukasz.momot@hotmail.com>
2023-06-22 13:26:39 +0200

    Add example list component

---
+++ src/app/examples/example-list/example-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-example-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './example-list.component.html',
+  styleUrls: ['./example-list.component.css']
+})
+export class ExampleListComponent {
+
+}
--- src/app/examples/index.ts
+++ src/app/examples/index.ts
@@ -1,5 +1,8 @@
+import { ExampleListComponent } from "./example-list/example-list.component";
 import { ExampleStandComponent } from "./example-stand/example-stand.component";
 
 export { ExampleStandComponent } from "./example-stand/example-stand.component";
+export { ExampleListComponent } from "./example-list/example-list.component";
 
-export const EXAMPLES =  [ExampleStandComponent]
+
+export const EXAMPLES =  [ExampleStandComponent, ExampleListComponent]
